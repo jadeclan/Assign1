853e4d42cf74e2cb6220fb60655ebcdac1fb2ddc
nwalker <nwalk957@mtroyal.ca>
2016-02-06 17:29:31 -0700

    fixed bug in card 3

--- javaScript/card3.js
+++ javaScript/card3.js
@@ -1,19 +1,16 @@
 var $cSelection = $('#cSelection');
 
 $cSelection.on('change',function() {
-    var $continentBox = $('#continentBox');
-
-    for (var i = $continentBox.length; i > 1; i--) {
-        i.deleteRow(i - 1);
-    }
-    $continentBox.attr("class", "");
+    var $tableBody = $('#contBody');
+    $tableBody.empty();
+    $('#continentBox').attr("class", "");
 
     for (i = 0; i < continentData.length; i++) {
         if (continentData[i].continentName == $cSelection.val() ){
             var row = $('<tr>');
             $('<td>').html(continentData[i].countryName).appendTo(row);
             $('<td>').html(continentData[i].hits).appendTo(row);
-            row.appendTo( $('#contBody') );
+            row.appendTo( $tableBody );
         }
     }
 
