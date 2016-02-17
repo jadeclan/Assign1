7f4d30b88cfab8ea1371807fa024aba94275dec5
spitrola <spitrola27@gmail.com>
2016-02-16 23:34:03 -0700

    continent file

---
+++ app/models/Continent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Application;
+
+use Framework\Model;
+
+class ContinentsModel extends Model
+{
+    public function getAll()
+    {
+        $sql = "SELECT ContinentCode, ContinentName FROM continents ORDER BY ContinentName";
+
+        return $this->db->query($sql)->fetchAll();
+    }
+
+    public function getByContinentCode($continentCode)
+    {
+        $sql = "SELECT * FROM continents WHERE ContinentCode = " . $this->db->quote(strtoupper($continentCode));
+
+        return $this->db->query($sql)->fetchAll();
+    }
+}
